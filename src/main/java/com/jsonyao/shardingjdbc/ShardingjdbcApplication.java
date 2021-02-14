ca6d93c4b727a578e3c92f80f9518d8eb2fde597
yaocs2 <yaocs2@midea.com>
2021-02-14 10:30:01 +0800

    sharding-jdbc-test> 资源初始化

---
+++ src/main/java/com/jsonyao/shardingjdbc/ShardingjdbcApplication.java
@@ -0,0 +1,15 @@
+package com.jsonyao.shardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ShardingJDBC测试应用
+ */
+@SpringBootApplication
+public class ShardingjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingjdbcApplication.class, args);
+    }
+}
